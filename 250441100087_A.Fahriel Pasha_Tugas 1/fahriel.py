e106f15b732d07d00e982475a777ee379665ddce
Eldharren01 <pashadharren@gmail.com>
2025-09-15 21:32:12 +0700

    Add files via upload

---
+++ 250441100087_A.Fahriel Pasha_Tugas 1/fahriel.py
@@ -0,0 +1,35 @@
+a = 8
+b = 3
+
+hasil = a + b
+
+print(hasil)
+print(a + b)
+print(8 + 7)
+print(8 + 7)
+
+pengurangan = a - b
+
+print(pengurangan)
+print(a - b)
+
+perkalian = a * b
+
+print(perkalian)
+
+pembagian = a / b
+
+print(pembagian)
+
+pembagian_bulat = 10 // 3
+
+print(pembagian_bulat)
+
+modulus = 10 % 3
+
+print(modulus)
+
+pangkat = a**9
+
+print(pangkat)
+
